5fbad0437cd24965184a611c0aaba25ad0659344
Yeray Callero <yeray@geosophic.com>
2014-05-26 20:07:31 +0200

    Files pending from previous commit.

---
+++ src/com/relayr/core/api/Relayr_APICommons.java
@@ -0,0 +1,7 @@
+package com.relayr.core.api;
+
+public class Relayr_APICommons {
+
+	public static String DEFAULT_REDIRECTION_URI = "http://www.relayrdummyredirection.com";
+
+}
---
+++ src/com/relayr/core/event_listeners/LoginEventListener.java
@@ -0,0 +1,7 @@
+package com.relayr.core.event_listeners;
+
+public interface LoginEventListener {
+
+	public void onUserLoggedInSuccessfully();
+	public void onUserLoggedOutSuccessfully();
+}
