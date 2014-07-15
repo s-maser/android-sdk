27cbc542dae9f879183b082cfbcbb777b87de872
Hugo <hudomju@gmail.com>
2014-07-15 16:53:33 +0200

    Add app id property

--- src/com/relayr/core/settings/Relayr_SDKSettings.java
+++ src/com/relayr/core/settings/Relayr_SDKSettings.java
@@ -1,21 +1,23 @@
 package com.relayr.core.settings;
 
+import com.relayr.Relayr_Application;
+import com.relayr.core.error.Relayr_Exception;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
-
-import com.relayr.Relayr_Application;
-import com.relayr.core.error.Relayr_Exception;
 
 public final class Relayr_SDKSettings {
 
 	private static String VERSION = "0.0.1";
 	private static String clientId;
 	private static String clientSecret;
+    private static String appId;
 
-	private static String propertiesFileName = "relayrsdk.properties";
-	private static String clientIdTag = "clientId";
-	private static String clientSecretTag = "clientSecret";
+	private static final String PROPERTIES_FILE_NAME = "relayrsdk.properties";
+    private static final String PROPERTIES_KEY_CLIENT_ID = "clientId";
+    private static final String PROPERTIES_KEY_CLIENT_SECRET = "clientSecret";
+    private static final String PROPERTIES_KEY_APP_ID = "appId";
 
 	public static String getVersion() {
 		return VERSION;
@@ -37,16 +39,27 @@ public final class Relayr_SDKSettings {
 		Relayr_SDKSettings.clientSecret = clientSecret;
 	}
 
-	public static boolean checkConfigValues() throws Exception {
+    private static void setAppId(String appId) {
+        Relayr_SDKSettings.appId = appId;
+    }
+
+    public static String getAppId() {
+        return appId;
+    }
+
+    public static boolean checkConfigValues() throws Exception {
 		try {
-			InputStream inputStream = Relayr_Application.currentActivity().getAssets().open(propertiesFileName);
+			InputStream inputStream =
+                    Relayr_Application.currentActivity().getAssets().open(PROPERTIES_FILE_NAME);
 			Properties relayrProperties = new Properties();
 			relayrProperties.load(inputStream);
 			inputStream.close();
-			String clientId = relayrProperties.getProperty(clientIdTag);
+			String clientId = relayrProperties.getProperty(PROPERTIES_KEY_CLIENT_ID);
 			Relayr_SDKSettings.setClientId(clientId);
-			String clientSecret = relayrProperties.getProperty(clientSecretTag);
+			String clientSecret = relayrProperties.getProperty(PROPERTIES_KEY_CLIENT_SECRET);
 			Relayr_SDKSettings.setClientSecret(clientSecret);
+            String appId = relayrProperties.getProperty(PROPERTIES_KEY_APP_ID);
+            Relayr_SDKSettings.setAppId(appId);
 			return true;
 		} catch (IOException e) {
 			throw new Relayr_Exception("Can't find properties file");
