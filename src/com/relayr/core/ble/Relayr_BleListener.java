8a2901b2144bde00243cd837311e711650f992b7
Hugo <hudomju@gmail.com>
2014-07-24 17:35:51 +0200

    Simplify

--- src/com/relayr/core/ble/Relayr_BleListener.java
+++ src/com/relayr/core/ble/Relayr_BleListener.java
@@ -1,4 +1,4 @@
- package com.relayr.core.ble;
+package com.relayr.core.ble;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
@@ -14,109 +14,106 @@ import com.relayr.core.ble.device.Relayr_BLEDevice;
 import com.relayr.core.ble.device.Relayr_BLEDeviceStatus;
 import com.relayr.core.ble.device.Relayr_BLEDeviceType;
 
+import static com.relayr.core.ble.device.Relayr_BLEDeviceType.Unknown;
+
 @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
 public class Relayr_BleListener {
 
-	private static BluetoothAdapter bluetoothAdapter;
-	private static Relayr_BleDevicesScanner scanner;
-	public static Relayr_DeviceManager discoveredDevices;
+    private static final String TAG = Relayr_BleListener.class.toString();
 
-	private final static int REQUEST_ENABLE_BT = 1;
+    private static Relayr_BleDevicesScanner scanner;
+    public static Relayr_DeviceManager discoveredDevices;
 
-	static {
-		scanner = null;
-		discoveredDevices = new Relayr_DeviceManager();
-	}
+    private final static int REQUEST_ENABLE_BT = 1;
 
-	public static boolean init() {
-		Log.d(Relayr_BleListener.class.toString(), "BleListener init called");
-		if (Relayr_Commons.isSDK18()) {
-			Log.d(Relayr_BleListener.class.toString(), "BleListener init starting");
-			final int bleStatus = Relayr_BleUtils.getBleStatus(Relayr_Application.currentActivity());
-			switch (bleStatus) {
-			case Relayr_BleUtils.STATUS_BLE_NOT_AVAILABLE:
-			case Relayr_BleUtils.STATUS_BLUETOOTH_NOT_AVAILABLE:
-				Log.d(Relayr_BleListener.class.toString(), "Bluethooth not available");
-				return false;
-			default:
-				Activity currentActivity = Relayr_Application.currentActivity();
-				bluetoothAdapter = Relayr_BleUtils.getBluetoothAdapter(currentActivity);
-				if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
-				    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-				    currentActivity.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-				    return false;
-				}
-				scanner = new Relayr_BleDevicesScanner(bluetoothAdapter, new BluetoothAdapter.LeScanCallback() {
-					@Override
-					public void onLeScan(final BluetoothDevice device, final int rssi, byte[] scanRecord) {
-						if (!discoveredDevices.isDeviceDiscovered(device.getAddress()) && (Relayr_BLEDeviceType.getDeviceType(device.getName()) != Relayr_BLEDeviceType.Unknown)) {
-							Log.d(Relayr_BleListener.class.toString(), "New device: "+ device.getName() + " [" + device.getAddress() + "]");
-							Relayr_BLEDevice relayrDevice = new Relayr_BLEDevice(device);
-							discoveredDevices.addNewDevice(relayrDevice.getAddress(), null);
-							relayrDevice.setStatus(Relayr_BLEDeviceStatus.CONFIGURING);
-							Log.d(Relayr_BleListener.class.toString(), "Device cofiguration start: "+ relayrDevice.toString());
-							relayrDevice.connect();
-						}
-					}
-				});
-				scanner.setScanPeriod(7000);
-				return true;
-			}
-		}
-		return false;
-	}
+    static {
+        scanner = null;
+        discoveredDevices = new Relayr_DeviceManager();
+    }
 
-	public static void start() {
-		if (Relayr_Commons.isSDK18()) {
-			if (scanner != null) {
-				if (!scanner.isScanning()) {
-					discoveredDevices.clearDiscoveredDevices();
-					scanner.start();
-					Log.d(Relayr_BleListener.class.toString(), "Scanner start: " + scanner.toString());
-				}
-			} else {
-				if (init()) {
-					scanner.start();
-					Log.d(Relayr_BleListener.class.toString(), "New scanner start: " + scanner.toString());
-				}
+    public static boolean init() {
+        Log.d(TAG, "BleListener init called");
+        if (!Relayr_Commons.isSDK18()) return false;
+        Log.d(TAG, "BleListener init starting");
+        final int bleStatus = Relayr_BleUtils.getBleStatus(Relayr_Application.currentActivity());
+        switch (bleStatus) {
+            case Relayr_BleUtils.STATUS_BLE_NOT_AVAILABLE:
+            case Relayr_BleUtils.STATUS_BLUETOOTH_NOT_AVAILABLE:
+                Log.d(TAG, "Bluethooth not available");
+                return false;
+            default:
+                Activity currentActivity = Relayr_Application.currentActivity();
+                BluetoothAdapter bluetoothAdapter = Relayr_BleUtils.getBluetoothAdapter(currentActivity);
+                if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
+                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    currentActivity.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                    return false;
+                }
+                scanner = new Relayr_BleDevicesScanner(bluetoothAdapter, new BluetoothAdapter.LeScanCallback() {
 
-			}
-		}
-	}
+                    private boolean hasANewDeviceBeenDiscovered(BluetoothDevice device) {
+                        return !discoveredDevices.isDeviceDiscovered(device.getAddress()) &&
+                                Relayr_BLEDeviceType.getDeviceType(device.getName()) != Unknown;
+                    }
 
-	public static void refresh() {
-		if (Relayr_Commons.isSDK18()) {
-			if (scanner != null) {
-				discoveredDevices.refreshDiscoveredDevices();
-				if (!scanner.isScanning()) {
-					scanner.start();
-					Log.d(Relayr_BleListener.class.toString(), "Scanner start: " + scanner.toString());
-				}
-			} else {
-				if (init()) {
-					scanner.start();
-					Log.d(Relayr_BleListener.class.toString(), "New scanner start: " + scanner.toString());
-				}
-			}
-		}
-	}
+                    @Override
+                    public void onLeScan(BluetoothDevice device, final int rssi, byte[] scanRecord) {
+                        if (hasANewDeviceBeenDiscovered(device)) {
+                            Log.d(TAG, "New device: "+ device.getName() + " [" + device.getAddress() + "]");
+                            Relayr_BLEDevice relayrDevice = new Relayr_BLEDevice(device);
+                            discoveredDevices.addNewDevice(relayrDevice.getAddress(), null);
+                            relayrDevice.setStatus(Relayr_BLEDeviceStatus.CONFIGURING);
+                            Log.d(TAG, "Device cofiguration start: "+ relayrDevice.toString());
+                            relayrDevice.connect();
+                        }
+                    }
+                });
+                scanner.setScanPeriod(7000);
+                return true;
+        }
+    }
 
-	public static void stop() {
-		if (Relayr_Commons.isSDK18()) {
-			scanner.stop();
-			Log.d(Relayr_BleListener.class.toString(), "Scanner stop: " + scanner.toString());
-		}
-	}
+    public static void start() {
+        if (!Relayr_Commons.isSDK18()) return;
+        if (scanner != null) {
+            if (!scanner.isScanning()) {
+                discoveredDevices.clearDiscoveredDevices();
+                startScanner();
+            }
+        } else {
+            startScanner();
+        }
+    }
 
-	public static boolean isScanning() {
-		if (Relayr_Commons.isSDK18()) {
-			return scanner == null? false : scanner.isScanning();
-		} else {
-			return false;
-		}
-	}
+    private static void startScanner() {
+        if (!init()) return;
+        scanner.start();
+        Log.d(TAG, "New scanner start: " + scanner.toString());
+    }
 
-	public static Relayr_DeviceManager getDeviceManager() {
-		return discoveredDevices;
-	}
+    public static void refresh() {
+        if (!Relayr_Commons.isSDK18()) return;
+        if (scanner != null) {
+            discoveredDevices.refreshDiscoveredDevices();
+            if (!scanner.isScanning()) {
+                startScanner();
+            }
+        } else {
+            startScanner();
+        }
+    }
+
+    public static void stop() {
+        if (!Relayr_Commons.isSDK18()) return;
+        scanner.stop();
+        Log.d(TAG, "Scanner stop: " + scanner.toString());
+    }
+
+    public static boolean isScanning() {
+        return Relayr_Commons.isSDK18() && scanner != null && scanner.isScanning();
+    }
+
+    public static Relayr_DeviceManager getDeviceManager() {
+        return discoveredDevices;
+    }
 }
