7f100d43910af955329ec4277e763543aaccbef4
Hugo <hudomju@gmail.com>
2014-10-02 22:12:50 +0200

    Refactoring

--- tests/src/androidTest/java/io/relayr/ble/service/BaseServiceTest.java
+++ tests/src/androidTest/java/io/relayr/ble/service/BaseServiceTest.java
@@ -33,17 +33,65 @@ import static org.mockito.Mockito.when;
 @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
 public class BaseServiceTest {
 
+    private static final int EXPECTED_BATTERY = 100;
+    private static final String EXPECTED_FIRMWARE = "1.2.3";
+    private static final String EXPECTED_HARDWARE = "1.0.0";
+    private static final String EXPECTED_MANUFACTURER = "Relayr";
+
     private BluetoothDevice device;
+    private BluetoothGattCharacteristic batteryCharacteristic;
+    private BluetoothGattCharacteristic firmwareCharacteristic;
+    private BluetoothGattCharacteristic hardwareCharacteristic;
+    private BluetoothGattCharacteristic manufacturerCharacteristic;
+    private BluetoothGatt gatt;
+    private BluetoothGattReceiver receiver;
+    private BaseService baseService;
 
     @Before public void initialise() {
         device = mock(BluetoothDevice.class);
+
+        BluetoothGattService batteryService = mock(BluetoothGattService.class);
+        when(batteryService.getUuid()).thenReturn(fromString("0000180f-0000-1000-8000-00805f9b34fb"));
+
+        batteryCharacteristic = mock(BluetoothGattCharacteristic.class);
+        when(batteryCharacteristic.getValue()).thenReturn(new byte[] {EXPECTED_BATTERY});
+        when(batteryCharacteristic.getUuid()).thenReturn(fromString("00002a19-0000-1000-8000-00805f9b34fb"));
+
+        List<BluetoothGattCharacteristic> batteryCharacteristics = Arrays.asList(batteryCharacteristic);
+        when(batteryService.getCharacteristics()).thenReturn(batteryCharacteristics);
+
+
+        BluetoothGattService deviceInfoService = mock(BluetoothGattService.class);
+        when(deviceInfoService.getUuid()).thenReturn(fromString("0000180A-0000-1000-8000-00805f9b34fb"));
+
+        firmwareCharacteristic = mock(BluetoothGattCharacteristic.class);
+        when(firmwareCharacteristic.getStringValue(0)).thenReturn(EXPECTED_FIRMWARE);
+        when(firmwareCharacteristic.getValue()).thenReturn(EXPECTED_FIRMWARE.getBytes());
+        when(firmwareCharacteristic.getUuid()).thenReturn(fromString("00002A26-0000-1000-8000-00805f9b34fb"));
+
+        hardwareCharacteristic = mock(BluetoothGattCharacteristic.class);
+        when(hardwareCharacteristic.getStringValue(0)).thenReturn(EXPECTED_HARDWARE);
+        when(hardwareCharacteristic.getUuid()).thenReturn(fromString("00002a27-0000-1000-8000-00805f9b34fb"));
+
+        manufacturerCharacteristic = mock(BluetoothGattCharacteristic.class);
+        when(manufacturerCharacteristic.getStringValue(0)).thenReturn(EXPECTED_MANUFACTURER);
+        when(manufacturerCharacteristic.getUuid()).thenReturn(fromString("00002a29-0000-1000-8000-00805f9b34fb"));
+
+        List<BluetoothGattCharacteristic> characteristics = Arrays.asList(
+                firmwareCharacteristic, hardwareCharacteristic, manufacturerCharacteristic);
+        when(deviceInfoService.getCharacteristics()).thenReturn(characteristics);
+
+        List<BluetoothGattService> services = Arrays.asList(deviceInfoService, batteryService);
+
+        gatt = mock(BluetoothGatt.class);
+        when(gatt.getServices()).thenReturn(services);
+        receiver = new BluetoothGattReceiver();
+        baseService = new BaseService(device, gatt, receiver);
     }
 
     @Test public void connectTest() {
         @SuppressWarnings("unchecked")
         Observer<BaseService> observer = mock(Observer.class);
-
-        final BluetoothGattReceiver receiver = new BluetoothGattReceiver();
 
         BaseService
                 .doConnect(device, receiver)
@@ -64,8 +112,6 @@ public class BaseServiceTest {
     @Test public void disconnectTest() {
         @SuppressWarnings("unchecked")
         Observer<BluetoothGatt> observer = mock(Observer.class);
-
-        final BluetoothGattReceiver receiver = new BluetoothGattReceiver();
 
         BaseService
                 .doConnect(device, receiver)
@@ -92,115 +138,43 @@ public class BaseServiceTest {
     }
 
     @Test public void getBatteryLevelTest() {
-        BluetoothGattService service = mock(BluetoothGattService.class);
-        when(service.getUuid()).thenReturn(fromString("0000180f-0000-1000-8000-00805f9b34fb"));
-
-        BluetoothGattCharacteristic characteristic = mock(BluetoothGattCharacteristic.class);
-        when(characteristic.getValue()).thenReturn(new byte[] {0x64});
-        when(characteristic.getUuid()).thenReturn(fromString("00002a19-0000-1000-8000-00805f9b34fb"));
-
-        List<BluetoothGattCharacteristic> characteristics = Arrays.asList(characteristic);
-        when(service.getCharacteristics()).thenReturn(characteristics);
-
-        List<BluetoothGattService> services = Arrays.asList(service);
-
-        BluetoothGatt gatt = mock(BluetoothGatt.class);
-        when(gatt.getServices()).thenReturn(services);
-
-        BluetoothGattReceiver receiver = new BluetoothGattReceiver();
-        BaseService baseService = new BaseService(device, gatt, receiver);
         @SuppressWarnings("unchecked")
         Observer<? super Integer> observer = mock(Observer.class);
         baseService
                 .getBatteryLevel()
                 .subscribe(observer);
-        receiver.onCharacteristicRead(gatt, characteristic, GATT_SUCCESS);
-        verify(observer).onNext(100);
+        receiver.onCharacteristicRead(gatt, batteryCharacteristic, GATT_SUCCESS);
+        verify(observer).onNext(EXPECTED_BATTERY);
     }
 
     @Test public void getFirmwareVersionTest() {
-        BluetoothGattService service = mock(BluetoothGattService.class);
-        when(service.getUuid()).thenReturn(fromString("0000180A-0000-1000-8000-00805f9b34fb"));
-
-        BluetoothGattCharacteristic characteristic = mock(BluetoothGattCharacteristic.class);
-        String expected = "Relayr";
-        when(characteristic.getStringValue(0)).thenReturn(expected);
-        when(characteristic.getValue()).thenReturn(expected.getBytes());
-        when(characteristic.getUuid()).thenReturn(fromString("00002A26-0000-1000-8000-00805f9b34fb"));
-
-        List<BluetoothGattCharacteristic> characteristics = Arrays.asList(characteristic);
-        when(service.getCharacteristics()).thenReturn(characteristics);
-
-        List<BluetoothGattService> services = Arrays.asList(service);
-
-        BluetoothGatt gatt = mock(BluetoothGatt.class);
-        when(gatt.getServices()).thenReturn(services);
-
-        BluetoothGattReceiver receiver = new BluetoothGattReceiver();
-        BaseService baseService = new BaseService(device, gatt, receiver);
         @SuppressWarnings("unchecked")
         Observer<? super String> observer = mock(Observer.class);
         baseService
                 .getFirmwareVersion()
                 .subscribe(observer);
-        receiver.onCharacteristicRead(gatt, characteristic, GATT_SUCCESS);
-        verify(observer).onNext(expected);
+        receiver.onCharacteristicRead(gatt, firmwareCharacteristic, GATT_SUCCESS);
+        verify(observer).onNext(EXPECTED_FIRMWARE);
     }
 
     @Test public void getHardwareVersionTest() {
-        BluetoothGattService service = mock(BluetoothGattService.class);
-        when(service.getUuid()).thenReturn(fromString("0000180A-0000-1000-8000-00805f9b34fb"));
-
-        BluetoothGattCharacteristic characteristic = mock(BluetoothGattCharacteristic.class);
-        String expected = "1.0.0";
-        when(characteristic.getStringValue(0)).thenReturn(expected);
-        when(characteristic.getUuid()).thenReturn(fromString("00002a27-0000-1000-8000-00805f9b34fb"));
-
-        List<BluetoothGattCharacteristic> characteristics = Arrays.asList(characteristic);
-        when(service.getCharacteristics()).thenReturn(characteristics);
-
-        List<BluetoothGattService> services = Arrays.asList(service);
-
-        BluetoothGatt gatt = mock(BluetoothGatt.class);
-        when(gatt.getServices()).thenReturn(services);
-
-        BluetoothGattReceiver receiver = new BluetoothGattReceiver();
-        BaseService baseService = new BaseService(device, gatt, receiver);
         @SuppressWarnings("unchecked")
         Observer<? super String> observer = mock(Observer.class);
         baseService
                 .getHardwareVersion()
                 .subscribe(observer);
-        receiver.onCharacteristicRead(gatt, characteristic, GATT_SUCCESS);
-        verify(observer).onNext(expected);
+        receiver.onCharacteristicRead(gatt, hardwareCharacteristic, GATT_SUCCESS);
+        verify(observer).onNext(EXPECTED_HARDWARE);
     }
 
     @Test public void getManufacturerTest() {
-        BluetoothGattService service = mock(BluetoothGattService.class);
-        when(service.getUuid()).thenReturn(fromString("0000180A-0000-1000-8000-00805f9b34fb"));
-
-        BluetoothGattCharacteristic characteristic = mock(BluetoothGattCharacteristic.class);
-        String expected = "Relayr";
-        when(characteristic.getStringValue(0)).thenReturn(expected);
-        when(characteristic.getUuid()).thenReturn(fromString("00002a29-0000-1000-8000-00805f9b34fb"));
-
-        List<BluetoothGattCharacteristic> characteristics = Arrays.asList(characteristic);
-        when(service.getCharacteristics()).thenReturn(characteristics);
-
-        List<BluetoothGattService> services = Arrays.asList(service);
-
-        BluetoothGatt gatt = mock(BluetoothGatt.class);
-        when(gatt.getServices()).thenReturn(services);
-
-        BluetoothGattReceiver receiver = new BluetoothGattReceiver();
-        BaseService baseService = new BaseService(device, gatt, receiver);
         @SuppressWarnings("unchecked")
         Observer<? super String> observer = mock(Observer.class);
         baseService
                 .getManufacturer()
                 .subscribe(observer);
-        receiver.onCharacteristicRead(gatt, characteristic, GATT_SUCCESS);
-        verify(observer).onNext(expected);
+        receiver.onCharacteristicRead(gatt, manufacturerCharacteristic, GATT_SUCCESS);
+        verify(observer).onNext(EXPECTED_MANUFACTURER);
     }
 
 }
