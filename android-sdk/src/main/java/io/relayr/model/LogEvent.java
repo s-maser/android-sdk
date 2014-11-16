cba054afc113102469a66f90e4c515559bceae92
Hugo <hudomju@gmail.com>
2014-11-16 09:31:01 -0800

    Log timestamp in ISO format

--- android-sdk/src/main/java/io/relayr/model/LogEvent.java
+++ android-sdk/src/main/java/io/relayr/model/LogEvent.java
@@ -1,13 +1,24 @@
 package io.relayr.model;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class LogEvent {
 
+    private static final DateFormat sDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+
+    static {
+        sDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+    }
+
     private final String message;
-    private final long timestamp;
+    private final String timestamp;
 
     public LogEvent(String message) {
         this.message = message;
-        this.timestamp = System.currentTimeMillis();
+        this.timestamp = sDateFormat.format(new Date());
     }
 
 }
