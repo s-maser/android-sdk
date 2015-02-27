1d65447b390aedd9c2ced5ec2ad2d1fe88a9a400
Bernard Pletikosa <bernard.pletikosa@gmail.com>
2015-02-27 16:22:23 +0100

    Add path as part of ModelReading entity.

--- android-sdk/src/main/java/io/relayr/model/ModelReading.java
+++ android-sdk/src/main/java/io/relayr/model/ModelReading.java
@@ -3,13 +3,15 @@ package io.relayr.model;
 public class ModelReading {
 
     final public String meaning;
+    final public String path;
     final public Object unit;
     final public Object minimum;
     final public Object maximum;
     final public Object precision;
 
-    public ModelReading(String meaning, Object unit, Object minimum, Object maximum, Object precision) {
+    public ModelReading(String meaning, String path, Object unit, Object minimum, Object maximum, Object precision) {
         this.meaning = meaning;
+        this.path = path;
         this.unit = unit;
         this.minimum = minimum;
         this.maximum = maximum;
@@ -20,10 +22,11 @@ public class ModelReading {
     public String toString() {
         return "ModelReading{" +
                 "meaning='" + meaning + '\'' +
-                ", unit='" + unit + '\'' +
-                ", minimum='" + minimum + '\'' +
-                ", maximum='" + maximum + '\'' +
-                ", precision='" + precision + '\'' +
+                ", path='" + path + '\'' +
+                ", unit=" + unit +
+                ", minimum=" + minimum +
+                ", maximum=" + maximum +
+                ", precision=" + precision +
                 '}';
     }
 }
