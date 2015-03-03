f1a538753bd3bc48cb5fd42887ce433549d7999e
Hugo <hudomju@gmail.com>
2015-03-03 16:04:36 +0100

    Shortcut to send command from a TransmitterDevice

--- android-sdk/src/main/java/io/relayr/model/TransmitterDevice.java
+++ android-sdk/src/main/java/io/relayr/model/TransmitterDevice.java
@@ -39,5 +39,10 @@ public class TransmitterDevice extends Transmitter {
     public void unSubscribeToCloudReadings() {
         RelayrSdk.getWebSocketClient().unSubscribe(id);
     }
+
+    /** Sends a command to the this device */
+    public Observable<Void> sendCommand(Command command) {
+        return RelayrSdk.getRelayrApi().sendCommand(id, command);
+    }
     
 }
